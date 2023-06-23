d4b1f545888e37126828e02d89ac8bf6aae712da
Jyotiraditya-lal <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-06-23 11:53:00 +0530

    Create ExpenseDetails.js

---
+++ EcpenseDetails.js
@@ -0,0 +1,15 @@
+function ExpenseDetails(props){
+    const title=props.title;
+    const amount=props.amount;
+    const location=props.location;
+
+    return(
+        <div>
+            <div>{title}</div>
+            <div>{location}</div>
+            <div>{amount}</div>
+        </div>
+    )
+}
+
+export default ExpenseDetails;
