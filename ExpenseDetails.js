8d047f47193089a2bc56139f6e6d518133c2e50c
Jyotiraditya-lal <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-06-28 11:33:16 +0530

    Delete button added

--- ExpenseDetails.js
+++ ExpenseDetails.js
@@ -3,12 +3,18 @@ function ExpenseDetails(props){
     const amount=props.amount;
     const location=props.location;
 
+    function DeleteHandler(){
+        console.log("Delete!!")
+    }
+
     return(
         <div>
             <div>{title}</div>
             <div>{location}</div>
             <div>{amount}</div>
+            <button onClick={DeleteHandler}>Delete Expense</button>
         </div>
+        
     )
 }
 
